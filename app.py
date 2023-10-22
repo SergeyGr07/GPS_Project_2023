92d5372af7826b9d5af0b5cd67732f59dfad6fcc
SergeyGr07 <s_grudinin@Sergi>
2023-10-22 22:04:49 +0800

    add_bluprint

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from routes import tracker
 
 app = Flask(__name__)
 
+app.register_blueprint(tracker, url_prefix='/tracker/')
+
 
 @app.route('/')
-def hello_world():
-    return 'Привет, мир!'
+def index():
+    return 'Go to <a href="http://127.0.0.1:5000/tracker/">Tracker</a>'
 
 
 if __name__ == '__main__':
