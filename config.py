07d8c4c27cb2952938ab4833a7c048a1dcf8e3b4
SergeyGr07 <s_grudinin@Sergi>
2023-10-22 22:35:39 +0800

    add_logger

--- config.py
+++ config.py
@@ -1,15 +1,49 @@
+from loguru._logger import Core as _Core
+from loguru._logger import Logger
+import os
+
+LOG_PATH = "/log"
+
+
+def add_logger(logger_name: str, script_name: str):
+    logger_name = Logger(
+        core=_Core(),
+        exception=None,
+        depth=0,
+        record=False,
+        lazy=False,
+        colors=False,
+        raw=False,
+        capture=True,
+        patcher=None,
+        extra={},
+    )
+
+    logger_name.add(f"{LOG_PATH}/{script_name}.log", level="DEBUG", rotation="9:00")
+    return logger_name
+
+
+script_name = os.path.splitext(os.path.basename(__file__))[0]
+logger = add_logger(f'logger_{script_name}', script_name)
+
+
 def is_point_in_path(x: float, y: float, poly) -> bool:
-    num = len(poly)
-    j = num - 1
-    c = False
-    for i in range(num):
-        if (x == poly[i][0]) and (y == poly[i][1]):
-            return True
-        if ((poly[i][1] > y) != (poly[j][1] > y)):
-            slope = (x - poly[i][0]) * (poly[j][1] - poly[i][1]) - (poly[j][0] - poly[i][0]) * (y - poly[i][1])
-            if slope == 0:
+    try:
+        num = len(poly)
+        j = num - 1
+        c = False
+        for i in range(num):
+            if (x == poly[i][0]) and (y == poly[i][1]):
                 return True
-            if (slope < 0) != (poly[j][1] < poly[i][1]):
-                c = not c
-        j = i
-    return c
+            if ((poly[i][1] > y) != (poly[j][1] > y)):
+                slope = (x - poly[i][0]) * (poly[j][1] - poly[i][1]) - (poly[j][0] - poly[i][0]) * (y - poly[i][1])
+                if slope == 0:
+                    return True
+                if (slope < 0) != (poly[j][1] < poly[i][1]):
+                    c = not c
+            j = i
+        return c
+
+    except Exception as e:
+        logger.info(f"An error occurred: {e}")
+        return False
--- routes/map_routing/map_routing.py
+++ routes/map_routing/map_routing.py
@@ -1,6 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify
 import folium
 from config import is_point_in_path
+import traceback
+import os
+import requests
+from logger_core import add_logger
+
+script_name = os.path.splitext(os.path.basename(__file__))[0]
+logger = add_logger(f'logger_{script_name}', script_name)
+
 
 tracker = Blueprint("map_route", __name__)
 
@@ -87,35 +95,80 @@ def index():
 
 @tracker.route('/map')
 def map():
-    p1 = [
-        (52.25668206898288, 104.26340472198405), (52.25817582347088, 104.27125256195853),
-        (52.25873596843354, 104.27128029284205), (52.25924518498766, 104.27247272082326),
-        (52.261638424473404, 104.27128029284205), (52.26471040783642, 104.26753662359545),
-        (52.26416731024287, 104.25943920567232), (52.25668206898288, 104.26340472198405)]
-    marker1 = [52.26153, 104.25855]
+    try:
+        p1 = [
+            (52.25668206898288, 104.26340472198405), (52.25817582347088, 104.27125256195853),
+            (52.25873596843354, 104.27128029284205), (52.25924518498766, 104.27247272082326),
+            (52.261638424473404, 104.27128029284205), (52.26471040783642, 104.26753662359545),
+            (52.26416731024287, 104.25943920567232), (52.25668206898288, 104.26340472198405)]
+        marker1 = [52.26153, 104.25855]
 
-    m = folium.Map(location=[52.257529268365516, 104.26505193126633], zoom_start=15)
-    m.get_root().html.add_child(folium.Element('<style>.leaflet-control-attribution {display:none;}</style>'))
+        m = folium.Map(location=[52.257529268365516, 104.26505193126633], zoom_start=15)
+        m.get_root().html.add_child(folium.Element('<style>.leaflet-control-attribution {display:none;}</style>'))
 
-    shapesLayer = folium.FeatureGroup(name="warning").add_to(m)
+        shapesLayer = folium.FeatureGroup(name="warning").add_to(m)
 
-    poin = is_point_in_path(marker1[0], marker1[1], p1)
-    if not poin:
-        color_p1 = "red"
-    else:
-        color_p1 = "green"
+        poin = is_point_in_path(marker1[0], marker1[1], p1)
+        if not poin:
+            color_p1 = "red"
+        else:
+            color_p1 = "green"
 
-    folium.Polygon(
-        p1,
-        weight=2,
-        fill_color=color_p1,
-        fill_opacity=0.2).add_to(shapesLayer)
+        folium.Polygon(
+            p1,
+            weight=2,
+            fill_color=color_p1,
+            fill_opacity=0.2).add_to(shapesLayer)
 
-    tooltip = "Click me!"
-    folium.Marker(location=marker1, popup="<i>Backend dev</i>", tooltip=tooltip).add_to(m)
+        tooltip = "Click me!"
+        folium.Marker(location=marker1, popup="<i>Backend dev</i>", tooltip=tooltip).add_to(m)
 
-    m = m.get_root().render()
-    context = {
-        'm': m
-    }
-    return render_template('main.html', **context)
+        # m = m.get_root().render()
+        # context = {
+        #     'm': m
+        # }
+        m_json = m.to_json()
+        return jsonify({'map': m_json})
+
+    except KeyError as e:
+        logger.debug(e)
+        logger.debug(traceback.format_exc())
+
+        return jsonify({"message": "Ошибка доступа",
+                        "data": None,
+                        "error": "Unauthorized"}), 401
+
+    except requests.exceptions.HTTPError as e:
+
+        logger.debug(traceback.format_exc())
+        logger.debug(e)
+
+        return jsonify({"message": "При загрузке данных произошла ошибка",
+                        "data": None,
+                        "error": "HTTP Error"}), 500
+
+    except requests.exceptions.ConnectionError as e:
+
+        logger.debug(traceback.format_exc())
+        logger.debug(e)
+
+        return jsonify({"message": "Ошибка подключения",
+                        "data": None,
+                        "error": "Connection Error"}), 500
+
+    except requests.exceptions.Timeout as e:
+
+        logger.debug(traceback.format_exc())
+        logger.debug(e)
+
+        return jsonify({"message": "Время запроса истекло",
+                        "data": None,
+                        "error": "Timeout Error"}), 500
+    except Exception as e:
+
+        logger.debug(traceback.format_exc())
+        logger.debug(e)
+
+        return jsonify({"message": "Ошибка при загрузке данных",
+                        "data": None,
+                        "error": str(e)}), 500
