36953be9bc8cc9a15f8ba1c8828b52eec2964b30
MrRomainzZ <roman.shalin87@gmail.com>
2023-11-27 19:47:40 +0800

    Map JS Update

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,18 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+let config = {
+  minZoom: 7,
+  maxZoom: 18,
+	attributionControl: false,
+};
+
+// magnification with which the map will start
+const zoom = 16;
+// co-ordinates
+const lat = 52.2613;
+const lng = 104.2655;
+
+const map = L.map('map', config).setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
